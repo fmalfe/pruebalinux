c31f4b716828b1f084a388ddc654a4945d27ff66
fmalfe <fmalfetano@uade.edu.ar>
2024-12-26 22:19:41 -0300

    first commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+def hola_mundo():
+    print("Hola, mundo!")
+
+def suma_dos_mas_dos():
+    return 2 + 2
+
+if __name__ == "__main__":
+    hola_mundo()
+    resultado = suma_dos_mas_dos()
+    print(f"2 + 2 = {resultado}")
---
+++ helloworld_dir.py
@@ -0,0 +1,13 @@
+import os
+
+def hola_mundo():
+    print("Hola, mundo!")
+
+def suma_dos_mas_dos():
+    return 2 + 2
+
+if __name__ == "__main__":
+    hola_mundo()
+    resultado = suma_dos_mas_dos()
+    print(f"2 + 2 = {resultado}")
+    print(f"Este archivo se encuentra en: {os.path.abspath(__file__)}")
